57e28e3f9366edf959cd29c0532d42520c00d874
Julian Rosse <julian@helixbass.net>
2026-01-12 17:15:32 -0500

    working on request

--- tests/tcp.rs
+++ tests/tcp.rs
@@ -4,7 +4,10 @@ use std::time::Duration;
 use rkyv::{rancor, string::ArchivedString, vec::ArchivedVec};
 use smol_str::ToSmolStr;
 use tempfile::tempdir;
-use tokio::{net::TcpStream, time::timeout};
+use tokio::{
+    net::TcpStream,
+    time::{sleep, Instant},
+};
 use uuid::Uuid;
 
 use brunhilde::{client, tcp, AppendRow, RowWithoutEventId, RowsRequest};
@@ -18,13 +21,7 @@ async fn test_tcp() {
         .spawn()
         .unwrap();
 
-    let client_tcp_stream = timeout(
-        Duration::from_millis(5000),
-        TcpStream::connect("127.0.0.1:8421"),
-    )
-    .await
-    .unwrap()
-    .unwrap();
+    let client_tcp_stream = connect().await;
 
     let table_id = Uuid::new_v4();
 
@@ -34,7 +31,7 @@ async fn test_tcp() {
     let row_uuid = Uuid::new_v4();
     let payload: Vec<String> = vec!["foo".to_owned(), "bar".to_owned()];
 
-    let client_tcp_stream = TcpStream::connect("127.0.0.1:8421").await.unwrap();
+    let client_tcp_stream = connect().await;
 
     let response = client::request(
         tcp::Request::Request(
@@ -55,7 +52,7 @@ async fn test_tcp() {
     .await;
     let event_id = response.as_append_row();
 
-    let client_tcp_stream = TcpStream::connect("127.0.0.1:8421").await.unwrap();
+    let client_tcp_stream = connect().await;
 
     let response = client::request(
         tcp::Request::Request(RowsRequest::new(table_id).into()),
@@ -75,4 +72,21 @@ async fn test_tcp() {
     assert_eq!(rows[0].event_id, event_id);
 
     running_server.kill().unwrap();
+}
+
+async fn connect() -> TcpStream {
+    let timeout = Instant::now() + Duration::from_millis(5000);
+    loop {
+        if Instant::now() > timeout {
+            panic!("failed to connect");
+        }
+        match TcpStream::connect("127.0.0.1:8421").await {
+            Ok(tcp_stream) => {
+                return tcp_stream;
+            }
+            Err(_) => {
+                sleep(Duration::from_millis(100)).await;
+            }
+        }
+    }
 }
